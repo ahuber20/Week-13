3a70e1a34b369f91b3560fece444e09101c225ee
Adom <adomhuber20@gmail.com>
2024-07-24 17:33:25 -0600

    spring boot pet park files

---
+++ pet-parks/src/main/java/pet/park/PetParkApplication.java
@@ -0,0 +1,14 @@
+package pet.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParkApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PetParkApplication.class, args);
+	}
+
+}
